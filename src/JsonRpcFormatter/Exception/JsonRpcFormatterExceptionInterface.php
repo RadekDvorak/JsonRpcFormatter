2203325635cc63c3bc13f0b9e2caaa12d8ec3987
Radek Dvořák <radek.dvorak@gmail.com>
2012-04-13 00:16:04 +0200

    Exceptions: per project exception interface

---
+++ src/JsonRpcFormatter/Exception/JsonRpcFormatterExceptionInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace JsonRpcFormatter\Exception;
+
+/**
+ * Interface indicates implementing exceptions come from this module
+ *
+ * @author Radek Dvořák <radek.dvorak@gmail.com>
+ */
+interface JsonRpcFormatterExceptionInterface
+{
+	
+}
+
